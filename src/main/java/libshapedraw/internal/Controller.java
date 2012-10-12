d758438bd84547593fc72b869d5387c27fad8501
Ben Cartwright <bencvt@gmail.com>
2012-10-12 18:39:35 -0400

    fix sky lighting bug caused by new rendering hook location

--- src/main/java/libshapedraw/internal/Controller.java
+++ src/main/java/libshapedraw/internal/Controller.java
@@ -185,7 +185,6 @@ public class Controller {
         GL11.glPopMatrix();
         GL11.glDepthMask(true);
         GL11.glDepthFunc(origDepthFunc);
-        GL11.glEnable(GL11.GL_LIGHTING);
         GL11.glEnable(GL11.GL_TEXTURE_2D);
         GL11.glDisable(GL11.GL_BLEND);
     }
